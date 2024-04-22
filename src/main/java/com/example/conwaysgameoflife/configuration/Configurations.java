1222dcd891d9b1755a6b5a1f27b3b2a21136c30f
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-04-21 23:03:02 -0300

    feat: configuration values class

---
+++ src/main/java/com/example/conwaysgameoflife/configuration/Configurations.java
@@ -0,0 +1,18 @@
+package com.example.conwaysgameoflife.configuration;
+
+public class Configurations {
+    public static int rows = 3;
+    public static int columns = 3;
+    public static int generationsPerMinute = 120;
+
+    public static final int MAX_ROWS = 38;
+    public static final int MAX_COLUMNS = 38;
+    public static final int MIN_ROWS = 3;
+    public static final int MIN_COLUMNS = 3;
+
+    public static final int MAX_INITIAL_LIVE_CELLS = 50;
+    public static final int MIN_INITIAL_LIVE_CELLS = 10;
+
+    public static final int MAX_GENERATIONS_PER_MINUTE = 15;
+    public static final int MIN_GENERATIONS_PER_MINUTE = 450;
+}
