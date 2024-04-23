5e69277ca8b4122b42573360bfdd59a90c991fa3
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-04-23 18:51:05 -0300

    fix: ideal initial live cells min and max values

--- src/main/java/com/example/conwaysgameoflife/configuration/Configurations.java
+++ src/main/java/com/example/conwaysgameoflife/configuration/Configurations.java
@@ -10,8 +10,8 @@ public class Configurations {
     public static final int MIN_ROWS = 3;
     public static final int MIN_COLUMNS = 3;
 
-    public static final int MAX_INITIAL_LIVE_CELLS = 50;
-    public static final int MIN_INITIAL_LIVE_CELLS = 10;
+    public static final int MAX_INITIAL_LIVE_CELLS = 900;
+    public static final int MIN_INITIAL_LIVE_CELLS = 5;
 
     public static final int MAX_GENERATIONS_PER_MINUTE = 450;
     public static final int MIN_GENERATIONS_PER_MINUTE = 15;
